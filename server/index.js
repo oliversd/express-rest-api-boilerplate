95c22b36af9bc5640a5ed0ecf1ef4fc4a9ac329f
Olivers De Abreu <oliversd@gmail.com>
2017-03-06 20:19:54 -0300

    Add debug module

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import http from 'http';
+import debugModule from 'debug';
+
+const debug = debugModule('app');
+
+debug('Starting Express Boilerplate...');
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!!\n');
+  res.end('Hello World!!!\n');
 }).listen(3000, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:3000/');
+debug('Server running at http://127.0.0.1:3000/');
