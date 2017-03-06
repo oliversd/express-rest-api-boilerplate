558ca1485d5591589540326a9747475ca691afe7
Olivers De Abreu <oliversd@gmail.com>
2017-03-06 19:33:46 -0300

    Add eslint

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import http from 'http';
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!!\n');
 }).listen(3000, '127.0.0.1');
 
