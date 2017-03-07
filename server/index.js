3e93dd1d43d3ea429e830084f6ecf6ea6a66b3a8
Olivers De Abreu <oliversd@gmail.com>
2017-03-07 15:25:19 -0300

    Add dotenv and set first enviroment variable

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 import http from 'http';
 import debugModule from 'debug';
+import { config } from 'dotenv';
 
+// Load Enviroment variables from .env file
+// this .env file must be in the root folder
+config();
+
+// Set debug namespace for our app
 const debug = debugModule('app');
+
+// Set server port
+const apiPort = process.env.API_PORT || 3000;
+
 
 debug('Starting Express Boilerplate...');
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!!!\n');
-}).listen(3000, '127.0.0.1');
+}).listen(apiPort, '127.0.0.1');
 
-debug('Server running at http://127.0.0.1:3000/');
+debug(`Server running at http://127.0.0.1:${apiPort}/`);
