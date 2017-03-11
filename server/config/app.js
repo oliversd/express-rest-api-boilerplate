47016e3f85a0f0aeb734008a27cbd95a7b931ddb
Olivers De Abreu <oliversd@gmail.com>
2017-03-11 14:31:38 -0300

    Add packages and express configuration helmet, cors, body-parser, method-override

--- server/config/app.js
+++ server/config/app.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import routes from '../routes/health';
 
 const app = express();
 
+app.use(methodOverride());
+app.use(helmet()); // for a little security. https://github.com/helmetjs/helmet
+app.use(cors());
+
 app.use('/api', routes);
+
+// Configure bodyParser
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // use qs library https://github.com/expressjs/body-parser#extended
 
 app.use((req, res, next) => {
   res.status(404).json({ status: 'error', error: 'Sorry endpoint not found!' });
