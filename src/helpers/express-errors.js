af7fb731866388efe56fa390da7e7621f0964922
Luis Fuenmayor <luis.fuenmayor@engiefactory.com>
2017-08-31 12:57:22 -0300

    added lint:watch

--- src/helpers/express-errors.js
+++ src/helpers/express-errors.js
@@ -2,5 +2,5 @@
 // thanks to https://strongloop.com/strongblog/async-error-handling-expressjs-es7-promises-generators
 // and https://medium.freecodecamp.com/getting-started-with-async-await-b66385983875#.mq1zbgke6
 // catch any errors in the promise and either forward them to next(err) or ignore them
-export const catchErrors = fn => (req, res, next) => fn(req, res, next).catch(next);
+export const catchErrors = ce => (req, res, next) => ce(req, res, next).catch(next);
 export const ignoreErrors = fn => (req, res, next) => fn(req, res, next).catch(() => next());
