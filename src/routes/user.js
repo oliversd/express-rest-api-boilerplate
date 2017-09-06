5bf57abf3cd150e7ca22cada36a69c114bab8f9b
Luis Fuenmayor <luis.fuenmayor@engiefactory.com>
2017-09-06 14:31:26 -0300

    added authentication and tests

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,21 +1,24 @@
 import express from 'express';
-
+import passport from 'passport';
 import userController from '../controllers/user';
 import validateRequest from '../config/validator/param-validator';
 import { createUser, findUser, updateUser } from '../config/validator/user-schema';
 
 const router = express.Router();
 
-router.get('/', userController.listUsers);
+const requireAuth = passport.authenticate('bearer', {
+  session: false
+});
+
 
 router.post('/', validateRequest(createUser), userController.createUser);
-
-router.get('/:id', validateRequest(findUser), userController.getUser);
-
-router.put('/:id', validateRequest(updateUser), userController.updateUser);
-
 router.post('/forgot', userController.forgotPassword);
-
 router.get('/:id/reset', userController.resetPassword);
+
+router.get('/:id', requireAuth, validateRequest(findUser), userController.getUser);
+router.put('/:id', requireAuth, validateRequest(updateUser), userController.updateUser);
+
+router.get('/', requireAuth, userController.listUsers);
+router.get('/me', requireAuth, userController.listUsers);
 
 export default router;
